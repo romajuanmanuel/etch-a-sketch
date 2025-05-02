e70ff97a0d5d16cbf45209411f6ae973b0c8a546
romaxolote <romajuanmanuel@gmail.com>
2025-05-02 19:45:27 -0300

    refactor: clean up HTML and CSS formatting for better readability

--- sketch.js
+++ sketch.js
@@ -33,10 +33,10 @@ function changeGridSize() {
 }
 
 function getRandomColor() {
- 
-    const r = Math.floor(Math.random() * 256);  
-    const g = Math.floor(Math.random() * 256);  
-    const b = Math.floor(Math.random() * 256); 
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     return `rgba(${r}, ${g}, ${b})`;
 
 }
